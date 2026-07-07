ddfa4f431e7dd506e8916f2354cfcc4903db3354
ruzmatovabdulazizxon-star <ruzmatovabdulazizxon@gmail.com>
2026-07-07 10:50:30 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const peerServer = ExpressPeerServer(server, { debug: true });
 app.use('/peerjs', peerServer);
 app.use(express.static('public'));
 
-// Bosh sahifaga kirganda 6 xonali tasodifiy raqam generatsiya qilib yo'naltirish
+// Bosh sahifaga kirganda avtomatik ravishda 6 xonali tasodifiy raqam generatsiya qilib yo'naltirish
 app.get('/', (req, res) => {
     const randomRoomId = Math.floor(100000 + Math.random() * 900000).toString();
     res.redirect(`/${randomRoomId}`);
